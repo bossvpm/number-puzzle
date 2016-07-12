d5a7e3dc33166d8263bd510ae67e3b7077e0be94
Vishnu Gp <gp@valmeeki.com>
2016-07-13 03:18:06 +0530

    Addedd facebook app code with login

--- index.php
+++ index.php
@@ -22,6 +22,28 @@
       xfbml      : true,
       version    : 'v2.6'
     });
+    function onLogin(response) {
+  if (response.status == 'connected') {
+    FB.api('/me?fields=first_name', function(data) {
+      var welcomeBlock = document.getElementById('fb-welcome');
+      welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+    });
+  }
+}
+
+FB.getLoginStatus(function(response) {
+  // Check login status on load, and if the user is
+  // already logged in, go directly to the welcome message.
+  if (response.status == 'connected') {
+    onLogin(response);
+  } else {
+    // Otherwise, show Login dialog first.
+    FB.login(function(response) {
+      onLogin(response);
+    }, {scope: 'user_friends, email'});
+  }
+});
+    
   };
 
   (function(d, s, id){
@@ -32,6 +54,8 @@
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 </script>
+  
+  <h1 id="fb-welcome"></h1>
 
     <h2>Number Puzzle</h2>
 
